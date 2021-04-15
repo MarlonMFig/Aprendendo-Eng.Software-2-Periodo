520c3a8cdd3e0e80634f58ee36369ad47804e8df
MarlonMFig <82554075+MarlonMFig@users.noreply.github.com>
2021-04-14 22:47:54 -0300

    Add files via upload

---
+++ Teste - Fahrenreit para Celsius DevC.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+	 int anoNasc, anoAtual, idade;
+	 cout << "Ano Atual:";
+	 cin >> anoAtual;
+	 cout << "Em que ano voce nasceu?";
+	 cin >> anoNasc;
+	 idade=(anoAtual-anoNasc);
+	 cout << "Você tem ou fara " << idade << "anos";
+}
+
+   
